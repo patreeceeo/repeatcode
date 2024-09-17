03d76476922821fe9c22c40737c05b7b1b3a40ee
Patrick Canfield <pscale01@gmail.com>
2024-09-16 17:07:54 -0700

    wip: loader + doctest

---
+++ loader.py
@@ -0,0 +1,10 @@
+class Loader:
+    """
+    The loader is used to load problems/topics from a list in which each
+    item is a tuple containing:
+        * the numeric ID from LeetCode
+        * the problem slug
+        * the difficulty rating
+        * a list of topics
+    """
+    
